4085c99e6e7813ddef1366e27f8bdb1e9d21a03f
mario carvalho <mario.carvalho.devpython@gmail.com>
2023-12-14 15:05:50 -0300

    condicional

--- Aulas/03-Avançando/conditional_render/src/components/ConditionalRender.jsx
+++ Aulas/03-Avançando/conditional_render/src/components/ConditionalRender.jsx
@@ -5,18 +5,24 @@ const ConditionalRender = () => {
 
     const [x] = useState(true);
     const num = 9
-
+    const [ name, setName ] = useState('Mario')
     return (
         <div>
             <h1>Conditional Render</h1>
             <h3>Isso será exibido</h3>
-            {x === true ? <p>"Exibiu"</p>  : <p>"Nao Exibiu"</p>}
-            {x && <p>X sendo atravez da consdicional simples</p>}
-            {num > 10 ? (
-                <p>Num e <strong>maior</strong> que 10</p>
-            ) : (
-                <p>Num e <strong>menor</strong> que 10</p>
-            )}
+                {x === true ? <p>"Exibiu"</p>  : <p>"Nao Exibiu"</p>}
+                {x && <p>X sendo atravez da consdicional simples</p>}
+            <h2>If ternario</h2>
+                {num > 10 ? (
+                    <p>Num e <strong>maior</strong> que 10</p>
+                ) : (
+                    <p>Num e <strong>menor</strong> que 10</p>
+                )}
+
+            <p>Meu nome é {name}</p>
+            <button onClick={(e) => (setName('Sergio'))}>Mudei o nome acima para Sergio</button>
+
+
         </div>
     )
 }

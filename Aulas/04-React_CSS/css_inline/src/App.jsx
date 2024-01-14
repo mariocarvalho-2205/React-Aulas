16e1a28a451657ad1849a168fd299b70dd87111c
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-01-14 19:46:10 -0300

    css inline

---
+++ Aulas/04-React_CSS/css_inline/src/App.jsx
@@ -0,0 +1,14 @@
+import CssInline from './CssInline'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>CSS Inline</h1>
+      <CssInline />
+    </>
+  )
+}
+
+export default App
---
+++ Aulas/04-React_CSS/css_inline/src/CssInline.jsx
@@ -0,0 +1,11 @@
+
+const CssInline = () => {
+  return (
+    <div>
+        <h2 style={{color: 'red', backgroundColor: 'blue', borderRadius: '15px'}}>CssInline</h2>
+
+    </div>
+  )
+}
+
+export default CssInline

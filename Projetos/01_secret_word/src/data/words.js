f74554c9d775b55f25374fc919ba4f97e4e56408
mario carvalho <mario.carvalho.devpython@gmail.com>
2024-01-26 11:19:08 -0300

    create object word

---
+++ Projetos/01_secret_word/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+  carro: ['Motor','Porta','Capô','Pneu','Antena'],
+  fruta: ['Banana','Maça','Pera','Morango','Abacaxi'],
+  corpo: ['Braço','Perna','Barriga','Pescoço','Olhos'],
+  computador: ['Mouse','Teclado','Monitor','CPU','Webcam'],
+  programacao: ['React','Javascript','Python','HTML','CSS'],
+  alimento: ['Arroz','Feijão','Macarrão','Carne','Frango'],
+};

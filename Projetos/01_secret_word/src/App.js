1ca9aa981d057fbc1cfff255a89a9a63e35b72cd
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-02-17 14:05:02 -0300

    reset game

--- Projetos/01_secret_word/src/App.js
+++ Projetos/01_secret_word/src/App.js
@@ -18,6 +18,8 @@ const stages = [
   { id: 3, name: "end" },
 ];
 
+const guessesQty = 3
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordList);
@@ -28,7 +30,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
@@ -92,11 +94,19 @@ function App() {
     }
   };
 
+  const clearLetterStates = () => {
+    setGuessedLetters([])
+    setWrongLetters([])
+  }
+
   useEffect(() => {
     // reset all states
 
     if (guesses <=0) {
+
       setGameStage(stages[2].name)
+
+      clearLetterStates()
     }
 
   }, [guesses])
@@ -105,6 +115,9 @@ function App() {
 
   // restart the game
   const retry = () => {
+    setScore(0)
+    setGuesses(guessesQty)
+
     setGameStage(stages[0].name);
   };
 

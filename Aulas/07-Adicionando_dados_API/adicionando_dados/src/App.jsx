b8ff1225c2641ff797345a30861e5da6b0c65d9b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-02-26 20:26:51 -0300

    create add data json

--- Aulas/07-Adicionando_dados_API/adicionando_dados/src/App.jsx
+++ Aulas/07-Adicionando_dados_API/adicionando_dados/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react"
 
 import './App.css'
-import FormCar from "./components/FormCar";
 
 function App() {
 
   const [cars, setCars ] = useState([]);
+
+  const [ marca, setMarca ] = useState("");
+  const [ modelo, setModelo ] = useState("");
+  const [ ano, setAno ] = useState("");
+  const [ price, setPrice ] = useState("");
 
   const url = "http://localhost:3000/cars";
 
@@ -22,6 +26,39 @@ function App() {
     console.log(cars);
   }, [])
  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // resgatando os dados do formulario
+    const car = {
+        marca,
+        modelo,
+        ano,
+        price
+    }
+
+    // configurando o methodo para POST
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(car)  // transforma os dados em json para einviar para o backend
+
+        
+    })
+    // carregamento dinamico para alimentar a pagina com os dados do backend
+    // instantanemente
+    const addedCar =  await res.json(); // pegamos o objeto json novamente para ser atualizado
+    setCars((prevCars) => [ ...prevCars, addedCar ])  // aqui adicionameo automaticamente 
+
+    // resetando as strings para um novo form
+    setMarca("");
+    setModelo("");
+    setAno("");
+    setPrice("");
+
+}
 
   return (
     <div className="container">
@@ -31,7 +68,27 @@ function App() {
           <li key={car.id}>{car.marca} - {car.modelo} - {car.ano} - R$ {car.price}</li>
         ))}
       </ul>
-      <FormCar />
+
+      <h2>New Car</h2>
+			<form className="form" onSubmit={handleSubmit}>
+				<label>
+					Marca:
+					<input type="text" value={marca} name="marca" onChange={(e) => setMarca(e.target.value)}/>
+				</label>
+				<label>
+					Modelo:
+					<input type="text" value={modelo} name="modelo" onChange={(e) => setModelo(e.target.value)}/>
+				</label>
+				<label>
+					Ano:
+					<input type="number" value={ano} name="ano" onChange={(e) => setAno(e.target.value)}/>
+				</label>
+				<label>
+					Preço:
+					<input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+				</label>
+                <input type="submit" value="Enviar" />
+			</form>
     </div>
   )
 }

c4a9325e6438376d59b2f49a8c6f497fcb050c7c
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-03-06 13:54:08 -0300

    loading post

---
+++ Aulas/07-Requisições_HTTP/loading_post/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+
+  return (
+    <>
+      <h1>Relação de Carros</h1>
+      <Form />
+    </>
+  )
+}
+
+export default App

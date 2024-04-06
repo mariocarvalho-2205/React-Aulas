9b2924360e7c11a6bd7be5ce16e449c30c1ab005
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-04-06 11:36:25 -0300

    use context

--- Aulas/09-Context/src/pages/About.jsx
+++ Aulas/09-Context/src/pages/About.jsx
@@ -1,9 +1,10 @@
-import { useContext } from 'react'
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from 'react'
+// import { CounterContext } from "../context/CounterContext";
 import ChangeCounter from "../../src/components/ChanggeCounter";
 
+import { useCounterContext } from "../hooks/useCounterContext"
 const About = () => {
-  const { counter } = useContext(CounterContext)
+  const { counter } = useCounterContext()
   return (
     <div>
       <h1>About</h1>

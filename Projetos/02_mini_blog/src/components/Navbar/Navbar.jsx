44b86e1f7f0f9ca57c3fc2e5f9fc73fda857c531
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-04-07 09:14:42 -0300

    navbar ok

--- Projetos/02_mini_blog/src/components/Navbar/Navbar.jsx
+++ Projetos/02_mini_blog/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to="/"
-            className={(isActive) => (isActive ? styles.active : "")}
+            className={({isActive}) => (isActive ? styles.active : "")}
           >
             Home
           </NavLink>
@@ -21,7 +21,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to="/about"
-            className={(isActive) => (isActive ? styles.active : "")}
+            className={({isActive}) => (isActive ? styles.active : "")}
           >
             Sobre
           </NavLink>

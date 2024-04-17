1bdf1d2fc83317d6fc61d0dd45bd70f503aafdce
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-04-16 21:38:08 -0300

    about finaly

--- Projetos/02_mini_blog/src/pages/About/About.jsx
+++ Projetos/02_mini_blog/src/pages/About/About.jsx
@@ -1,11 +1,14 @@
 // CSS
+import { Link } from "react-router-dom";
 import styles from "./About.module.css"
 import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>Sobre o Mini <span>Blog</span></h2>
+      <p>Este projeto consiste em um blog feito com React no front-end e Firebase no back-end.</p>
+      <Link className="btn" to="/posts/create" >Criar post</Link>
     </div>
   );
 };

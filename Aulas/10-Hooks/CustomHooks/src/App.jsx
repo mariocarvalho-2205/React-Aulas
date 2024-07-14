64871b0c11d5f9d703f35d371dc6514b609039b5
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-07-14 09:59:10 -0300

    hooks finally

--- Aulas/10-Hooks/CustomHooks/src/App.jsx
+++ Aulas/10-Hooks/CustomHooks/src/App.jsx
@@ -5,12 +5,12 @@ import './App.css'
 
 function App() {
   const [ number, setNumber ] = useState(0)
-  const previousValue = UsePrevious(number)
+  const previousValue = UsePrevious(number.toFixed(2))
 
   return (
     <>
       <h1>Hello World</h1>
-      <p>Atual: {number}</p>
+      <p>Atual: {number.toFixed(2)}</p>
       <p>Anterior: {previousValue}</p>
       <button onClick={() => setNumber(Math.random())}>Alterar</button>
     </>
--- Aulas/10-Hooks/CustomHooks/src/hooks/UsePrevious.jsx
+++ Aulas/10-Hooks/CustomHooks/src/hooks/UsePrevious.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useRef } from'react';
+import { useEffect, useRef, useDebugValue } from'react';
 
 export  const UsePrevious = (value) => {
     const ref = useRef();
+
+    useDebugValue("--- CUSTOM HOOK E USEDEBUGVALUE ---");
+    useDebugValue("O numero anterior e " + value);
 
     useEffect(() => {
         ref.current = value;

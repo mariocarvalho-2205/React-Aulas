7b0417bf23c2ad7e7c55c58fe3ccc3f9de554655
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-07-23 11:17:19 -0300

    validação de usuario

--- Projetos/03_React_Gram/backend/routes/UserRoutes.js
+++ Projetos/03_React_Gram/backend/routes/UserRoutes.js
@@ -4,7 +4,12 @@ const router = express.Router()
 // Controller
 const { register } = require("../controllers/UserController")
 
+// middlewares
+const validate = require("../middlewares/handleValidation")
+const { userCreateValidation } = require("../middlewares/userValidations")
+
 // Routes
-router.post("/register", register)
+// para a valçidação de usuario e necessario chamar a função userCreateValidation
+router.post("/register", userCreateValidation(), validate, register)
 
 module.exports = router

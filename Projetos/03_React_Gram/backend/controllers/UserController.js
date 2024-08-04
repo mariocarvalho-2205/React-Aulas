b496e853d46274a736e57540e570b466cedde12b
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-08-04 11:12:44 -0300

    create user

--- Projetos/03_React_Gram/backend/controllers/UserController.js
+++ Projetos/03_React_Gram/backend/controllers/UserController.js
@@ -13,8 +13,42 @@ const generateToken = (id) => {
 }
 
 // Register User and sign in
-const register = (req, res) => {
-    res.send('Registro')
+const register = async (req, res) => {
+    // res.send('Registro')
+    const { name, email, password } = req.body
+
+    // check if user exists
+    const user = await User.findOne({email})
+
+    if (user) {
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail"]})
+        return
+    }
+
+    // generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // if user was created sucessfully, return the token
+    // verifica se o usuario nao foi criado com sucesso
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde!"]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+
+
 }
 
 module.exports = {

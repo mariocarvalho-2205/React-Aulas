29f5b69b02c57dbcddb91d1f2e437fa5667e9c79
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-08-04 11:53:45 -0300

    login user

--- Projetos/03_React_Gram/backend/controllers/UserController.js
+++ Projetos/03_React_Gram/backend/controllers/UserController.js
@@ -1,56 +1,86 @@
-const User = require("../models/User")
+const User = require("../models/User");
 
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const jwtSecret = process.env.JWT_SECRET
+const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
 const generateToken = (id) => {
-    return jwt.sign({ id }, jwtSecret, {
-        expiresIn: "7d",
-    })
-}
+	return jwt.sign({ id }, jwtSecret, {
+		expiresIn: "7d",
+	});
+};
 
 // Register User and sign in
 const register = async (req, res) => {
-    // res.send('Registro')
-    const { name, email, password } = req.body
+	// res.send('Registro')
+	const { name, email, password } = req.body;
 
-    // check if user exists
+	// check if user exists
+	const user = await User.findOne({ email });
+
+	if (user) {
+		res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] });
+		return;
+	}
+
+	// generate password hash
+	const salt = await bcrypt.genSalt();
+	const passwordHash = await bcrypt.hash(password, salt);
+
+	// Create user
+	const newUser = await User.create({
+		name,
+		email,
+		password: passwordHash,
+	});
+
+	// if user was created sucessfully, return the token
+	// verifica se o usuario nao foi criado com sucesso
+	if (!newUser) {
+		res.status(422).json({
+			errors: ["Houve um erro, por favor tente mais tarde!"],
+		});
+		return;
+	}
+
+	res.status(201).json({
+		_id: newUser._id,
+		token: generateToken(newUser._id),
+	});
+};
+
+// Sign user in
+const login = async (req, res) => {
+    // res.send("Login")
+    const { email, password} = await req.body
     const user = await User.findOne({email})
 
-    if (user) {
-        res.status(422).json({errors: ["Por favor, utilize outro e-mail"]})
+    // check if user exists
+    if(!user) {
+        res.status(404).json({
+            errors: ["Usuario não encontrado."]
+        })
         return
     }
 
-    // generate password hash
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    // Create user
-    const newUser = await User.create({
-        name,
-        email,
-        password: passwordHash
-    })
-
-    // if user was created sucessfully, return the token
-    // verifica se o usuario nao foi criado com sucesso
-    if (!newUser) {
-        res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde!"]})
+    // check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Senha invalida!"]})
         return
     }
 
+    // return user with token
     res.status(201).json({
-        _id: newUser._id,
-        token: generateToken(newUser._id)
-    })
-
+		_id: user._id,
+        profileImage: user.profileImage,
+		token: generateToken(user._id),
+	});
 
 }
 
 module.exports = {
-    register,
-}
+	register,
+    login
+};
--- Projetos/03_React_Gram/backend/routes/UserRoutes.js
+++ Projetos/03_React_Gram/backend/routes/UserRoutes.js
@@ -2,14 +2,15 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const { register } = require("../controllers/UserController")
+const { register, login } = require("../controllers/UserController")
 
 // middlewares
 const validate = require("../middlewares/handleValidation")
-const { userCreateValidation } = require("../middlewares/userValidations")
+const { userCreateValidation, loginValidation } = require("../middlewares/userValidations")
 
 // Routes
 // para a valçidação de usuario e necessario chamar a função userCreateValidation
 router.post("/register", userCreateValidation(), validate, register)
+router.post("/login", loginValidation(), validate, login)
 
 module.exports = router

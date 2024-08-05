0c92ffc34919f9b147a6fe7d805e67fef3ddbe29
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-08-05 12:25:43 -0300

    auth

--- Projetos/03_React_Gram/backend/controllers/UserController.js
+++ Projetos/03_React_Gram/backend/controllers/UserController.js
@@ -80,7 +80,15 @@ const login = async (req, res) => {
 
 }
 
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+	const user = req.user
+
+	res.status(200).json(user)
+}
+
 module.exports = {
 	register,
-    login
+    login,
+	getCurrentUser
 };
--- Projetos/03_React_Gram/backend/routes/UserRoutes.js
+++ Projetos/03_React_Gram/backend/routes/UserRoutes.js
@@ -2,15 +2,17 @@ const express = require("express")
 const router = express.Router()
 
 // Controller
-const { register, login } = require("../controllers/UserController")
+const { register, login, getCurrentUser } = require("../controllers/UserController")
 
 // middlewares
 const validate = require("../middlewares/handleValidation")
 const { userCreateValidation, loginValidation } = require("../middlewares/userValidations")
+const authGuard = require("../middlewares/authGuard")
 
 // Routes
 // para a valçidação de usuario e necessario chamar a função userCreateValidation
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
+router.get('/profile', authGuard, getCurrentUser)
 
 module.exports = router

d046926b564dae1fe5350c6c68de34bb5be266a3
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-09-08 16:53:56 -0300

    config requisições

---
+++ Projetos/03_React_Gram/frontend/src/utils/config.jsx
@@ -0,0 +1,35 @@
+import { method } from "lodash"
+
+export const api = "http://loaclhost:5000/api"
+export const uploads = "http://localhost:5000/uploads"
+
+export const requestConfig = (method, data, token = null, image = null) => {
+    let config
+
+    if (image) {
+        config = {
+            method,
+            body: data,
+            headers: {},
+        }
+    } else if (method === "DELETE" || data === null) {
+        config = {
+            method,
+            headers: {},
+        }
+    } else {
+        config = {
+            method,
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    }
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+}

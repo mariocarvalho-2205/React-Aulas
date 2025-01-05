60cc53c5017f1b2bcafd6c4ef3f49f4e27ca29bf
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-01-05 15:09:16 -0300

    feat: creating photo by id and photo by user

--- Projetos/03_react_gram/backend/routes/PhotoRoutes.js
+++ Projetos/03_react_gram/backend/routes/PhotoRoutes.js
@@ -11,7 +11,10 @@ const authGuard = require("../middlewares/authGuard");
 const imageUpload = require('../middlewares/imageUpload');
 
 router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, PhotoController.insertPhoto)
-
+router.delete("/:id", authGuard, PhotoController.deletePhoto)
+router.get("/", authGuard, PhotoController.getAllPhotos)
+router.get("/user/:id", authGuard, PhotoController.getUserPhotos)
+router.get("/:id", authGuard, PhotoController.getPhotoById)
 
 
 module.exports = router

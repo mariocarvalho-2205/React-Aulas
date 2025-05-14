8b1e3c59f83e5ab89aabf0610238872cae47610a
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-14 16:44:39 -0300

    feat: creating login and validation

--- Projetos/03_react_gram/backend/Routes/UserRoutes.js
+++ Projetos/03_react_gram/backend/Routes/UserRoutes.js
@@ -2,13 +2,14 @@ import express from "express"
 const router = express.Router();
 
 // middlewares
-import {UserCreateSchema} from "../middlewares/userValidation.js"
+import {UserCreateSchema, UserLoginSchema} from "../middlewares/userValidation.js"
 
 import { validateRequest } from "../middlewares/handleValidation.js"
 
 
-import { register } from "../controllers/UserController.js"
+import { register, login } from "../controllers/UserController.js"
 
 router.post("/register", validateRequest(UserCreateSchema), register)
+router.post("/login", validateRequest(UserLoginSchema), login)
 
 export default router
--- Projetos/03_react_gram/backend/controllers/UserController.js
+++ Projetos/03_react_gram/backend/controllers/UserController.js
@@ -1,24 +1,91 @@
-import User from "../models/User.js"
-import bcrypt from "bcryptjs"
-import { json } from "express"
-import jwt from "jsonwebtoken"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { json } from "express";
+import jwt from "jsonwebtoken";
 
-
-const jwtSecret = process.env.JWT_SECRET
-console.log(jwtSecret ? "JWT_SECRET: Definido register (não mostrando por segurança)" : "Não definido")
+const jwtSecret = process.env.JWT_SECRET;
+console.log(
+  jwtSecret
+    ? "JWT_SECRET: Definido register (não mostrando por segurança)"
+    : "Não definido"
+);
 
 // Generate use token
 const generateToken = (id) => {
-    return jwt.sign({ id }, jwtSecret, {
-        expiresIn: "7d",
-    });
-    
+  return jwt.sign({ id }, jwtSecret, {
+    expiresIn: "7d",
+  });
 };
 
 export const register = async (req, res) => {
-    const { name, email, password} = await req.body
+  const { name, email, password } = req.body;
 
-    const user = await User.find({email})
+  const user = await User.findOne({ email });
+  if (user) {
+    return res
+      .status(422)
+      .json({ errors: ["Por favor, utilize outro email!"] });
+  }
 
-    return res.status(200).json({message: "Registro", user})
-}
+  // Generate password hash
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = {
+    name,
+    email,
+    password: passwordHash,
+  };
+
+  try {
+    const userCreate = await User.create(newUser);
+
+    // if user was created successfully, return the token
+    if (!userCreate) {
+      return res
+        .ststus(422)
+        .json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+    }
+
+    return res.status(201).json({
+      message: ["Usuario criado com sucesso!"],
+      _id: userCreate._id,
+      token: generateToken(userCreate._id), // aqui e gerado o token com a função criada no topo do codigo
+    });
+  } catch (error) {
+    res.status(500).json({ errors: ["Erro no servidor"] });
+  }
+
+  return res.status(200).json({ message: "Registro", passwordHash });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    // check if user exists
+    if (!user) {
+      return res.status(404).json({ errors: ["Usuario não encontrado."] });
+    }
+
+    // check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+      // aqui o bcrypt compara as senhas com a encriptada
+      return res.status(422).json({ errors: ["Senha invalida"] });
+    }
+
+    return res.status(201).json({
+      message: ["Usuario Logado com sucesso!"],
+      _id: user._id,
+      profileImage: user.profileImage,
+      token: generateToken(user._id), // aqui e gerado o token com a função criada no topo do codigo
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: ["Houve um erro, por favor tente mais tarde"],
+      error: error.errors,
+    });
+  }
+};
